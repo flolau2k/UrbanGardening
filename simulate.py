c1b13d3f5829ef76836f666b665f76c306db8591
kurz-m <m.kurz92@gmail.com>
2023-08-23 22:20:37 +0200

    initial commig

---
+++ simulate.py
@@ -0,0 +1,21 @@
+import simpy
+import math
+
+
+def temperature_curve(env):
+    time = 0
+    amplitute = 10
+    frequency = 0.07
+    offset = 15
+
+    while True:
+        extraTerm = math.sin(2 * math.pi * frequency * time)
+        temperature = offset + amplitute * extraTerm
+        yield env.timeout(1)
+        time += 1
+        print(f"Time: {time} hours, Temperature: {temperature:.2f}C")
+
+
+env = simpy.Environment()
+env.process(temperature_curve(env))
+env.run(until=24)
