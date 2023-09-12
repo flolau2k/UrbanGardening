9ab47d0e2938b195782720033a9bb98041702605
flolau2k <florian.lauer@live.de>
2023-09-12 12:39:37 +0200

    add simple influx client

---
+++ influx_client.py
@@ -0,0 +1,33 @@
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.flux_table import TableList
+from influxdb_client.client.write_api import SYNCHRONOUS
+import logging
+
+def get_logger(name):
+	logger = logging.getLogger(name)
+	logger.setLevel(logging.DEBUG)
+	ch = logging.StreamHandler()
+	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	ch.setFormatter(formatter)
+	logger.addHandler(ch)
+	return logger
+
+class influx_interface():
+	def __init__(self) -> None:
+		self.token = "DTWnhUGejpaKVVS7tJwraYK50sbzVhv-Q8to89TN6QJUFo5Oy0hRJPFvxX7LN5QWGkatY7Pwn0ntewbaPUcXnw=="
+		self.org = "urban"
+		self.url = "http://localhost:8086"
+		self.client = InfluxDBClient(url=self.url, token=self.token, org=self.org)
+		self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
+		self.query_api = self.client.query_api()
+		self.bucket = "gardening"
+		self.logger = get_logger("influx_interface")
+
+	def write(self, point:Point):
+		self.logger.info(f"writing point: {point.__str__()}")
+		self.write_api.write(bucket=self.bucket, org=self.org, record=point)
+
+	def get(self, query) -> TableList:
+		self.logger.info(f"processing query: {query}")
+		tables = self.query_api.query(query, org=self.org)
+		return tables
