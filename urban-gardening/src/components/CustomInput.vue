7eb40fb84546046ccf1ceeaa111f6894cd9d6833
RealConrad <wenzconrad@gmail.com>
2023-09-12 13:01:54 +0200

    fixed custom input emit not working

--- urban-gardening/src/components/CustomInput.vue
+++ urban-gardening/src/components/CustomInput.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
 
-let localValue: String  = " ";
-
-const emitInput = () => {
-  console.log(localValue);
-} 
-
 defineProps({
   name: String,
   label: String,
   isRequired: Boolean,
   inputType: String,
-
 })
+
+const emit = defineEmits(["emitValue"]);
+let localValue: String  = "";
+
+const emitInput = () => {
+  emit("emitValue", localValue);
+}
 
 </script>
 
@@ -29,7 +29,7 @@ defineProps({
    placeholder=""
 />
     <label
-      for={{label}}
+      :for=label
       class="absolute text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-darkBlack px-2 peer-focus:px-2 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
     >
       {{label}}
