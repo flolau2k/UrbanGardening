2a18cf5994b6e52e6856b114b2f7659a8d3769fd
RealConrad <wenzconrad@gmail.com>
2023-09-12 14:43:50 +0200

    wip

--- urban-gardening/src/components/CustomButton.vue
+++ urban-gardening/src/components/CustomButton.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-
 defineProps({
-  buttonName: String,
+  buttonName: String
 })
-
 </script>
 
 <template>
-  <button class="border px-4 py-2 text-base cursor-pointer hover:bg-green-300 hover:text-green-800 duration-300 ease-in-out rounded-md border-green-800 text-green-400" @click="$emit('customClick')">
-    {{buttonName}}
+  <button
+    class="border px-4 py-2 text-base cursor-pointer hover:bg-green-300 hover:text-green-800 duration-300 ease-in-out rounded-md border-green-800 text-green-400"
+    @click="$emit('customClick')"
+  >
+    {{ buttonName }}
   </button>
 </template>
--- urban-gardening/src/components/navbar.vue
+++ urban-gardening/src/components/navbar.vue
@@ -1,58 +1,34 @@
-<template>
-    <nav class="navbar">
-        <div class="navbar-left">
-            <h1 class="navbar-title">My App</h1>
-        </div>
-        <div class="navbar-middle">
-            <img class="navbar-logo" src="/path/to/logo.png" alt="Logo" />
-        </div>
-        <div class="navbar-right">
-            <CustomButton buttonName="Admin" @customClick="loginAsAdmin"></CustomButton>
-        </div>
-    </nav>
-</template>
+<script setup lang="ts">
+import UserIcon from './icons/UserIcon.vue';
+import ArrowIcon from "./icons/ArrowIcon.vue";
+import { ref } from 'vue';
 
+const isActive = ref<Boolean>(true);
 
-<script>
-import CustomButton from '@/components/CustomButton.vue';
-export default {
-    components: {
-        CustomButton,
-    },
-    methods: {
-        loginAsAdmin() {
-            alert('You are now logged in as admin');
-        },
-    },
-};
+const toggleNavbar = () => {
+    isActive.value = !isActive.value
+}
 </script>
 
-<style lang="postcss">
-@import "tailwindcss/base";
-@import "tailwindcss/components";
-@import "tailwindcss/utilities";
-
-.navbar {
-    @apply flex justify-between items-center bg-green-500 p-4;
-}
-
-.navbar-left {
-    @apply flex-1;
-}
-
-.navbar-middle {
-    @apply flex-1 flex justify-center;
-}
-
-.navbar-logo {
-    @apply h-12;
-}
-
-.navbar-right {
-    @apply flex-1 flex justify-end;
-}
-
-.navbar-admin-button {
-    @apply bg-white text-green-500 border-none px-4 py-2 rounded cursor-pointer;
-}
-</style>
+<template>
+    <div :class="isActive ? 'w-60' : 'w-20'" class="bg-green-500 p-4 text-white shadow-md h-screen relative transition-width duration-200">
+    <div @click="toggleNavbar" class="absolute -right-3 bg-green-500 rounded-full p-1 border">
+        <ArrowIcon :class="`w-4 h-4 text-light duration-300 ease-in-out ${isActive ? '' : '-rotate-180'}`" />
+    </div>
+      <div class="flex flex-col">
+          <div class="w-full text-center">
+            <div class="flex gap-x-2 items-center">
+                <img class="mx-auto w-10 h-10 rounded-full" src="../assets/navbar-logo.png" />
+                <a href="/" :class="`${isActive ? 'visible': 'invisible'} font-bold`">Urban Gardening</a>
+            </div>
+              <div class="bg-white my-5 w-full" style="height: 1px;"></div>
+              <a href="/" class="flex gap-x-5 items-center group">
+                <div>
+                    <UserIcon class="w-5 h-5 text-white" />
+                </div>
+                <div :class="isActive ? 'inline-block group-hover:underline' : 'invisible'">Profile</div>
+            </a>
+          </div>
+      </div>
+    </div>
+</template>
