8d56f4086809849018570bee858336e114f4ea96
pcazac <petru.cazac@yahoo.com>
2023-09-12 15:12:57 +0200

    wip

--- urban-gardening/src/components/status.vue
+++ urban-gardening/src/components/status.vue
@@ -2,15 +2,19 @@
     <div class="status-square">
         <div class="status-line">
             <i class="fas fa-leaf"></i>
-            <span class="status-text">Plants: {{ numPlants }}</span>
+            <span class="status-text">Plant: {{ kindPlants }}</span>
         </div>
         <div class="status-line">
             <i class="fas fa-tint"></i>
-            <span class="status-text">Water: {{ waterLevel }}</span>
+            <span class="status-text">Water: {{ waterLevel }}%</span>
+        </div>
+        <div class="status-line">
+            <i class="fas fa-tint"></i>
+            <span class="status-text">pH: {{ ph }}</span>
         </div>
         <div class="status-line">
             <i class="fas fa-sun"></i>
-            <span class="status-text">Sunlight: {{ sunlightLevel }}</span>
+            <span class="status-text">Total dissolved solids: {{ waterConductivity*0.64 }}(ppm)</span>
         </div>
         <div class="status-line">
             <i class="fas fa-thermometer-half"></i>
@@ -57,28 +61,28 @@ export default defineComponent({
 .status-square {
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 150px;
-    height: 150px;
-    background-color: #fff;
+    justify-content: left;
+    align-items: left;
+    width: 300px;
+    height: 250px;
+    background-color: #ffffff;
     border-radius: 10px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+    box-shadow: 0px 0px 10px rgba(28, 107, 176, 0.2);
 }
 
 .status-line {
     display: flex;
-    align-items: center;
+    align-items: left;
     margin: 5px;
 }
 
 .status-line i {
     margin-right: 10px;
-    color: #4caf50;
+    color: #47b2d6;
 }
 
 .status-text {
     font-size: 14px;
-    color: #333;
+    color: #101010;
 }
 </style>
