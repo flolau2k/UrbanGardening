cba54028cfd65a9316f752af5ee5a467bad6882a
RealConrad <wenzconrad@gmail.com>
2023-09-12 21:16:54 +0200

    added profile page

---
+++ urban-gardening/src/views/ProfileView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-full">
+    <div class="text-5xl text-white">
+      Not implemented.
+    </div>
+  </div>
+</template>
