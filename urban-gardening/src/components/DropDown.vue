67606902d35c39c710577588c298c4b323ccb5cf
RealConrad <wenzconrad@gmail.com>
2023-09-12 19:12:02 +0200

    finished dropdown component

--- urban-gardening/src/components/DropDown.vue
+++ urban-gardening/src/components/DropDown.vue
@@ -1,34 +1,54 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import ChevronIcon from "../components/icons/ChevronIcon.vue";
+import { DropDownData } from '../../types/DropDownData';
 
-  // defineProps({
-    // data: DropDownData[]
-  // })
+const props = defineProps<{ 
+    data: Array<DropDownData>,
+    dropdownName: String,
+  }>();
+const emit = defineEmits<{
+  (event: 'customClick', payload: DropDownData): void;
+}>();
 
-  const isActive = ref<Boolean>(false);
+const isActive = ref<boolean>(false);
 
-  const toggleDropDown = () => {
-    isActive.value = !isActive.value;
-  }
+const toggleDropDown = (): void => {
+  isActive.value = !isActive.value;
+}
+
+const emitDropdownItem = (item: DropDownData): void => {
+  emit("customClick", item);
+  toggleDropDown();
+}
+
 </script>
-<template>
 
+<template>
 <div class="z-20 relative">
-  <button @click="toggleDropDown" class="text-white bg-blue-700 hover:bg-green-800 bg-green-600 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center">
-    Dropdown button 
-    <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-    </svg>
+  <button @click="toggleDropDown" class="text-white hover:bg-green-800 bg-green-600 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5">
+    <div class="flex items-center gap-x-4">
+      <div>
+        {{ dropdownName }}
+      </div>
+      <div>
+        <ChevronIcon :class="`ease-in-out duration-200 ${isActive ? 'rotate-180' : ''}`" />
+      </div>
+    </div> 
   </button>
   
   <!-- Dropdown menu -->
-  <div v-if="isActive" id="dropdown" class="text-black top-11 absolute h-40 z-10  shadow w-44">
-      <div class="py-2 text-sm text-gray-700 bg-white rounded-lg">
-        <div>
-          <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+  <div v-if="isActive" id="dropdown" class="text-black top-11 absolute h-40 z-10 shadow w-44">
+    <div class="py-2 text-sm bg-white rounded-lg">
+      <div  v-if="props.data?.length > 0">
+        <div v-for="item in props.data" :key="item.id">
+          <div @click="emitDropdownItem(item)" class="block px-4 py-2 dark:hover:bg-gray-200 cursor-pointer">{{ item.itemName }}</div>
         </div>
       </div>
+      <div v-else>
+        No items to display!
+      </div>
+    </div>
   </div>
 </div>
-
 </template>
