871a63fd5145205cca4a6fa0e800f0b210aa9e4f
pcazac <petru.cazac@yahoo.com>
2023-09-12 13:47:10 +0200

    WIP

---
+++ urban-gardening/src/components/navbar.vue
@@ -0,0 +1,58 @@
+<template>
+    <nav class="navbar">
+        <div class="navbar-left">
+            <h1 class="navbar-title">My App</h1>
+        </div>
+        <div class="navbar-middle">
+            <img class="navbar-logo" src="/path/to/logo.png" alt="Logo" />
+        </div>
+        <div class="navbar-right">
+            <CustomButton buttonName="Admin" @customClick="loginAsAdmin"></CustomButton>
+        </div>
+    </nav>
+</template>
+
+
+<script>
+import CustomButton from '@/components/CustomButton.vue';
+export default {
+    components: {
+        CustomButton,
+    },
+    methods: {
+        loginAsAdmin() {
+            alert('You are now logged in as admin');
+        },
+    },
+};
+</script>
+
+<style lang="postcss">
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
+
+.navbar {
+    @apply flex justify-between items-center bg-green-500 p-4;
+}
+
+.navbar-left {
+    @apply flex-1;
+}
+
+.navbar-middle {
+    @apply flex-1 flex justify-center;
+}
+
+.navbar-logo {
+    @apply h-12;
+}
+
+.navbar-right {
+    @apply flex-1 flex justify-end;
+}
+
+.navbar-admin-button {
+    @apply bg-white text-green-500 border-none px-4 py-2 rounded cursor-pointer;
+}
+</style>
--- urban-gardening/tailwind.config.js
+++ urban-gardening/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        darkBlack: '#181818'
+        darkBlack: '#181818',
+        mindaro: '#d9ed92ff',
+        lightGreen: '#b5e48cff',
+        lightGreen2: '#99d98cff',
+        kindagreen: '#76c893ff',
+        greenBluish: '#52b69aff',
+        greenishBlue: '#34a0a4ff',
+        lightBlue: '#168aadff',
+        blue: '#1a759fff',
+        darkBlue: '#1e6091ff',
+        darkestBlue: '#184e77ff'
       }
     }
   },
