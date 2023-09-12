2b67578ef90462e8b0bc55994678aa8291d974b2
pcazac <petru.cazac@yahoo.com>
2023-09-12 15:37:11 +0200

    wip

--- urban-gardening/src/views/HomeView.vue
+++ urban-gardening/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 
 <script setup lang="ts">
   import StatusSquare from '../components/status.vue';
+  import CameraComponent from '../components/camera.vue';
 </script>
 <template>
   <StatusSquare />
+  <CameraComponent/>
 </template>
