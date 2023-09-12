ce23747637a8566751b041f32307ff5ca4e88215
RealConrad <wenzconrad@gmail.com>
2023-09-12 10:45:19 +0200

    intial commit

---
+++ urban-gardening/src/components/CustomButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+// const emits = defineEmits(['click']);
+
+</script>
+
+<template>
+  <button @click="$emit('click')">
+    click me
+  </button>
+</template>
---
+++ urban-gardening/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-red-500">
+    <!-- he -->
+  </div>
+</template>
