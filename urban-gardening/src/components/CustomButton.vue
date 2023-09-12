1e50c630b7f4f848650b59e78c71c3585c460899
RealConrad <wenzconrad@gmail.com>
2023-09-12 20:30:06 +0200

    added icons

--- urban-gardening/src/components/CustomButton.vue
+++ urban-gardening/src/components/CustomButton.vue
@@ -6,7 +6,7 @@ defineProps({
 
 <template>
   <button
-    class="border px-4 py-2 text-base cursor-pointer hover:bg-green-300 hover:text-green-800 duration-300 ease-in-out rounded-md border-green-800 text-green-400"
+    class="border px-4 py-2 text-base cursor-pointer hover:bg-green-300 hover:text-green-800 duration-300 ease-in-out rounded-lg border-green-800 text-green-400"
     @click="$emit('customClick')"
   >
     {{ buttonName }}
