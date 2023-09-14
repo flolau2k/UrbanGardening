2a98eb0f293cfde1c49943401267a377c3c0ee4f
RealConrad <wenzconrad@gmail.com>
2023-09-14 11:58:09 +0200

    fixed navbar

--- urban-gardening/src/components/NavBar.vue
+++ urban-gardening/src/components/NavBar.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="w-full py-2 px-10 text-white h-14 border-b fixed">
+  <div class="w-full py-2 px-10 text-white h-14 border-b fixed z-50 bg-teal-500">
     <div class="w-full flex items-center">
       <div class="flex flex-grow gap-x-2 items-center">
           <img class="w-10 h-10 rounded-full" src="../assets/navbar-logo.png" />
